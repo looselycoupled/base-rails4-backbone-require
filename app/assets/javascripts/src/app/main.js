70ce0fd3201629128251be95c5bd43fab527ab30
Allen Leis <allen.leis@gmail.com>
2014-07-03 00:04:13 -0400

    adds base requirejs, boostrap, etc. js libs

---
+++ app/assets/javascripts/src/app/main.js
@@ -0,0 +1,25 @@
+/**
+ *  main.js
+ *  description
+ *
+ *  Author:   Allen Leis <allen.leis@gmail.com>
+ *  Created:  Tue Jun 24 18:48:02 2014 -0400
+ *
+ *  Copyright (C) 2014 Tipsy Bear Studios
+ *  For license information, see LICENSE.txt
+ *
+ *  ID: main.js [] allen.leis@gmailcom $
+ */
+
+// JS Hint directives and strict mode
+/* globals define */
+'use strict';
+
+var dependencies = ['backbone'];
+
+define(dependencies, function(Backbone){
+
+  new Backbone.Model();
+
+});
+
